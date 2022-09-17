fa5eb40c3b29f0917e6ead577c6f187c450b292f
wshan2 <70585721+wshan2@users.noreply.github.com>
2022-09-17 13:09:10 -0400

    Update 1197. Minimum Knight Moves.py

--- 1197. Minimum Knight Moves.py
+++ 1197. Minimum Knight Moves.py
@@ -15,3 +15,29 @@ class Solution:
         return self.dfs(abs(x), abs(y))
         
     
+class Solution:
+   # BFS with time limit exceeded (TLE lol)
+    def bfs(self,x,y):
+        offsets = [(1, 2), (2, 1), (2, -1), (1, -2),
+                   (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
+        visited = set()
+        queue = deque([(0,0)])
+        steps = 0
+        
+        while queue:
+            curr_level_cnt = len(queue)
+            for i in range(curr_level_cnt):
+                curr_x, curr_y = queue.popleft()
+                if (curr_x, curr_y) == (x,y):
+                    return steps
+                
+                for offset_x, offset_y in offsets:
+                    next_x, next_y = curr_x + offset_x, curr_y + offset_y
+                    if (next_x, next_y) not in visited:
+                        visited.add((next_x, next_y))
+                        queue.append((next_x, next_y))
+        
+            steps += 1
+            
+    def minKnightMoves(self, x: int, y: int) -> int:
+        return self.bfs(x,y)
